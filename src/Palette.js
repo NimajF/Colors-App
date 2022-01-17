95e28a66e05a2b59021c41ff28a7f6e276b51028
NimajF <benjafran2000@gmail.com>
2022-01-17 18:58:39 +0100

    Added Palette component

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+function Palette(){
+    return(
+        <div className="Palette">
+            <div className="Palette-colors">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Palette;
