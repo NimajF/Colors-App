60ef2eabf3637d769e754164f75a88bea0fddaad
NimajF <benjafran2000@gmail.com>
2022-01-17 23:38:17 +0100

    Added colorHelpers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Palette from './Palette';
 import seedsColors from './seedsColors';
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedsColors[4]} />
+      <Palette palette={generatePalette(seedsColors[4])} />
     </div>
   );
 }
