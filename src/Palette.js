69adb5d233257fa794261781bd838126a6d4e634
NimajF <benjafran2000@gmail.com>
2022-01-17 19:32:45 +0100

    Added ColorBoxes

--- src/Palette.js
+++ src/Palette.js
@@ -1,10 +1,15 @@
 import react from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css"
 
-function Palette(){
+function Palette(props){
+    const colorBoxes = props.colors.map(color => (
+       <ColorBox background={color.color} name={color.name}/> 
+    ))
     return(
         <div className="Palette">
             <div className="Palette-colors">
-                
+                {colorBoxes}
             </div>
         </div>
     )
