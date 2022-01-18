e479bc2b7942fee1d295ca0a8c4737f0b1b881d5
NimajF <benjafran2000@gmail.com>
2022-01-18 17:11:48 +0100

    Installed react-router and fixed route issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Palette from './Palette';
 import seedsColors from './seedsColors';
 import { generatePalette } from "./colorHelpers";
+import { BrowserRouter as Router, Route, Routes, NavLink, useParams } from 'react-router-dom';
+import PaletteList from './PaletteList';
 
 function App() {
+
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedsColors[4])} />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<PaletteList palettes={seedsColors}/>}/>
+        <Route exact path="/palette/:id" element={<Palette/>} />
+      </Routes>
+      {/* <div className="App">
+        <Palette palette={generatePalette(seedsColors[4])} />
+      </div> */}
+    </Router>
+    
   );
 }
 
--- src/Palette.js
+++ src/Palette.js
@@ -1,15 +1,29 @@
 import react, { useState } from "react";
+import { useParams } from "react-router-dom";
 import ColorBox from "./ColorBox";
 import "./Palette.css"
 import Navbar from "./Navbar";
+import seedsColors from "./seedsColors";
+import { generatePalette } from "./colorHelpers";
+
+
 
 function Palette(props){
 
     const [level, setLevel] = useState(500)
     const [format, setFormat] = useState("hex")
 
+    const { id } = useParams();
 
-    const colorBoxes = props.palette.colors[level].map(color => (
+    const tryToFindPalette = id => {
+        return seedsColors.find(function(palette) {
+            return palette.id === id
+        })
+    }
+
+    const palette = generatePalette(tryToFindPalette(id))
+    console.log(palette)
+    const colorBoxes = palette.colors[level].map(color => (
        <ColorBox background={color[format]} name={color.name} key={color.id}/> 
     ))
 
@@ -28,7 +42,7 @@ function Palette(props){
                 {colorBoxes}
             </div>
             <footer className="Palette-footer">
-                <p>{props.palette.paletteName}</p>
+                <p>{palette.paletteName}</p>
             </footer>
         </div>
     )
