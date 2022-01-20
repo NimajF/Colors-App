e4da463b562f0331386416ebd2598cae543bf20b
NimajF <benjafran2000@gmail.com>
2022-01-20 21:40:16 +0100

    Started styling withStyles

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -3,30 +3,39 @@ import React from "react";
 
 
 const styles = {
-    main: {
-        backgroundColor: "purple",
-        border: "3px solid teal",
+    root: {
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        overflow: "hidden",
+        "&:hover": {
+            cursor: "pointer"
+        }
         
     },
-    secondary: {
-        backgroundColor: "pink",
-        "& h1": {
-            color: "white",
-            "& span": {
-                backgroundColor: "yellow"
-            }
-        }
+    colors: {
+        backgroundColor: "grey"
+    },
+    title: {
+        diplsay: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
     }
 }
 function MiniPalette(props){
     const {classes} = props;
     console.log(classes)
     return(
-        <div className={classes.main}>
-            <div>Mini Palette</div>
-            <section className={classes.secondary}>
-                <h1>Mini Palette <span>Wasker</span> </h1>
-            </section>
+        <div className={classes.root}>
+           <div className={classes.colors}></div>
+           <h5 className={classes.title}>{props.paletteName}</h5>
         </div>
 
     )
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,19 +1,64 @@
 import React from "react";
+import MiniPalette from "./MiniPalette";
 import { Link } from "react-router-dom";
+import { withStyles } from "@mui/styles";
+import { height } from "@mui/system";
 
+
+
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        border: "1px solid white"
+    },
+    nav: {
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%",
+        color: "white"
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gridGap: "5%"
+    }
+}
 
 function PaletteList(props){
-   
+   const { palettes, classes } = props
 
     return(
-        <div>
-            <h1>React Colors</h1>
-            {props.palettes.map(palette => (
-                <Link key={palette.id} to={`/palette/${palette.id}`} >{palette.paletteName}</Link>
-                // <h2>{palette.paletteName}</h2>
-            ))}
+        <div className={classes.root} >
+            <div className={classes.container} >
+                <nav className={classes.nav} >
+                    <h1>React Colors</h1>
+                </nav>
+                <div className={classes.palettes} >
+                    {palettes.map(palette => (
+                    <MiniPalette {...palette}/>
+                    // <Link key={palette.id} to={`/palette/${palette.id}`} >{palette.paletteName}</Link>
+                    // <h2>{palette.paletteName}</h2>
+                ))}
+                </div>
+            </div>
+            
+           
         </div>
     )
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
