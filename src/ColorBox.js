8c06c6ac29b00ba7fc87692ae0c2bf8ae0b0bbef
NimajF <benjafran2000@gmail.com>
2022-01-23 19:01:03 +0100

    Adjust text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 import "./ColorBox.css"
 
 function ColorBox({paletteId, name, background, id, showLink}){
@@ -14,23 +15,26 @@ function ColorBox({paletteId, name, background, id, showLink}){
         })
     }
 
+    const isDarkColor = chroma(background).luminance() <= 0.08;
+    const isLightColor = chroma(background).luminance() >= 0.7;
+
     return (
         <CopyToClipboard text={background} onCopy={changeCopyState}>
             <div className="ColorBox" style={{ background: background }} >
                 <div className={`copy-overlay ${copied && "show"}`} style={{ background: background }} />
                 <div className={`copied-msg ${copied && "show"}`}>
                     <h1>Copied!</h1>
-                    <p>{background}</p>
+                    <p className={isLightColor && "dark-text"} >{background}</p>
                 </div>
                 <div className="copy-container">
                     <div className="box-content">
-                        <span>{name}</span>
+                        <span className={isDarkColor && "light-text"} >{name}</span>
                     </div>
-                    <button className="copy-button">Copy</button>
+                    <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
                 </div>
                 {showLink && (
                         <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()} >
-                            <span className="see-more">More</span>
+                            <span className={`see-more ${isLightColor && "dark-text"}`}>More</span>
                         </Link>
                     )
                 }
