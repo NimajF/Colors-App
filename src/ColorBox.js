23473619361efdcaac57609bd43815f4aa2e3b01
NimajF <benjafran2000@gmail.com>
2022-01-24 14:22:23 +0100

    Finish refactor of ColorBox CSS

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -57,12 +57,70 @@ const styles = {
         background: "rgba(255, 255, 255, 0.3)",
         fontSize: "1rem",
         lineHeight: "30px",
-        color: "white",
         textTransform: "uppercase",
         border: "none",
         borderRadius: "3px",
         textDecoration: "none",
         opacity: 0
+    },
+    BoxContent: {
+        position: "absolute",
+        padding: "10px",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        letterSpacing: "1px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+    },
+    copyOverlay: {
+        opacity: "0",
+        zIndex: "0",
+        width: "100%",
+        height: "100%",
+        transition: "transform .6s ease-in-out",
+    },
+    showOverlay: {
+        opacity: "1",
+        zIndex: "10",
+        transform: "scale(50)",
+        position: "absolute",
+    },
+    copyMessage: {
+        position: "fixed",
+        left: "0",
+        right: "0",
+        top: "0",
+        bottom: "0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        opacity: "0",
+        color: "white",
+        "& h1": {
+            fontWeight: "400",
+            textShadow: "1px 2px 10px rgb(26, 26, 26)",
+            background: "rgba(255, 255, 255, 0.2)",
+            width: "100%",
+            textAlign: "center",
+            marginBottom: "0",
+            padding: "1rem",
+            textTransform: "uppercase",
+        },
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100",
+        }
+    },
+    showMessage: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all .3s ease-in-out",
+        transitionDelay: ".3s",
     }
 }
 
@@ -82,13 +140,13 @@ function ColorBox({paletteId, name, background, id, showingFullPalette, classes}
     return (
         <CopyToClipboard text={background} onCopy={changeCopyState}>
             <div className={classes.ColorBox} style={{ background: background }} >
-                <div className={`copy-overlay ${copied && "show"}`} style={{ background: background }} />
-                <div className={`copied-msg ${copied && "show"}`}>
+                <div className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} style={{ background: background }} />
+                <div className={`${classes.copyMessage} ${copied && classes.showMessage}`}>
                     <h1>Copied!</h1>
                     <p className={classes.copyText} >{background}</p>
                 </div>
                 <div className="copy-container">
-                    <div className="box-content">
+                    <div className={classes.BoxContent}>
                         <span className={classes.colorName} >{name}</span>
                     </div>
                     <button className={classes.copyButton}>Copy</button>
