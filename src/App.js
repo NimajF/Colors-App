39d85233fe417438024f35ccf8375eba4bcb4475
NimajF <benjafran2000@gmail.com>
2022-01-26 16:37:17 +0100

    Add SavePalette and fix new saved palettes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Palette from './Palette';
 import seedsColors from './seedsColors';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -5,15 +6,23 @@ import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
 
+
 function App() {
+const [newPalettes, setNewPalette] = useState(seedsColors)
+  const handleSavePalette = newSavedPalette => {
+    setNewPalette([...newPalettes, newSavedPalette])
+    
+    
+}
+
 
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<PaletteList palettes={seedsColors}/>}/>
-        <Route exact path="/palette/new" element={<NewPaletteForm/>}/>
-        <Route exact path="/palette/:id" element={<Palette/>} />
-        <Route path="/palette/:paletteId/:colorId" element={<SingleColorPalette/>} />
+        <Route exact path="/" element={<PaletteList palettes={newPalettes}/>}/>
+        <Route exact path="/palette/new" element={<NewPaletteForm handleSavePalette={handleSavePalette}/>}/>
+        <Route exact path="/palette/:id" element={<Palette palettes={newPalettes}/>} />
+        <Route path="/palette/:paletteId/:colorId" element={<SingleColorPalette palettes={newPalettes} />} />
       </Routes>
       {/* <div className="App">
         <Palette palette={generatePalette(seedsColors[4])} />
