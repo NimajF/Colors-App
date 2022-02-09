bb5278a2fc278d4cb1ec1b94e0c04b69db9a9c34
NimajF <benjafran2000@gmail.com>
2022-02-09 19:36:47 +0100

    Fix seed palettes error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
@@ -6,17 +6,18 @@ import SingleColorPalette from './SingleColorPalette';
 import NewPaletteForm from './NewPaletteForm';
 import NotFound from './NotFound';
 import customSeedColors from './customSeedColors';
+import seedsColors from './seedsColors';
 
 
 function App() {
   const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
   const [newPalettes, setNewPalette] = useState(savedPalettes || customSeedColors);
+  
+  const handleSavePalette = useCallback(newSavedPalette => {
+    setNewPalette(np => [...np, newSavedPalette]);
+  }, []);
 
-  const handleSavePalette = newSavedPalette => {
-    setNewPalette([...newPalettes, newSavedPalette]);
-};
-
-  const deletePalette = useCallback((paletteId) => {
+  const deletePalette = useCallback(paletteId => {
     setNewPalette(np => np.filter(palette => palette.id !== paletteId))
   }, [])
 
@@ -25,14 +26,14 @@ function App() {
   }, [newPalettes]);
 
 
-
+  const allPalettes = useMemo(() => [...newPalettes, ...seedsColors], [newPalettes])
 
   return (
     
       <Routes>
         <Route exact path="/" element={<PaletteList palettes={newPalettes} deletePalette={deletePalette} />}/>
         <Route exact path="/palette/new" element={<NewPaletteForm handleSavePalette={handleSavePalette} palettes={newPalettes} />}/>
-        <Route exact path="/palette/:id" element={<Palette palettes={newPalettes}/>} />
+        <Route exact path="/palette/:id" element={<Palette palettes={allPalettes}/>} />
         <Route path="/palette/:paletteId/:colorId" element={<SingleColorPalette palettes={newPalettes} />} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
